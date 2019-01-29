050ff226c64150660d82a8e131d165294daa5263
Lana Monson <44383292+LanaMonson@users.noreply.github.com>
2019-01-29 11:28:08 -0700

    Update counter.jsx

--- counter.jsx
+++ counter.jsx
@@ -2,20 +2,16 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = { 
-        count: 0,
-        imageUrl: 'https://picsum.photos/200'
-    };
-
-    styles = {
-        fontSize: 50,
-        fontWeight: 'bold'
+        count: 1,
     };
 
     render() { 
+        let classes = "badge m-2 badge-";
+        classes+= this.state.count === 0 ? "warning" : "primary";
+
         return(
             <div>
-                <img src={ this.state.imageUrl } alt=""/>
-                <span style={ this.styles } className="badge badge-primary m-2">{ this.formatCount() }</span>
+                <span className={classes}>{ this.formatCount() }</span>
                 <button className="btn btn-secondary btn-sm">Increment</button>
             </div>
         );
